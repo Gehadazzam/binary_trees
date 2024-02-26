bb911a5021486f4c1848e51e23b17610d536cc35
gehad <gehadgamalazzam@gmail.com>
2024-02-26 18:57:37 +0200

    order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,5 +7,11 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+	if (func == NULL
+		|| tree == NULL)
+		return;
+	func(tree->n);
+	/*recursioon*/
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
