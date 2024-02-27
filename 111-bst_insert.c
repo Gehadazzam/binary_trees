50a9caa0d33af61931e609c88602d1077360d9c8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-28 01:00:35 +0200

    binary

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,31 +1,31 @@
 #include "binary_trees.h"
 
 /**
-*bst_insert - inserts a value to a Binary searsh tree
-*@tree: tree to work in
-*@value: integer to be added
-*Return: pointer to the tree
-*/
+ * bst_insert - inserts a value to a Binary searsh tree
+ * @tree: tree to work in
+ * @value: integer to be added
+ * Return: pointer to the tree
+ */
 bst_t *bst_insert(bst_t **tree, int value)
 {
 	if (tree == NULL)
 		return (NULL);
 
 	if (*tree == NULL)
-        return (*tree = binary_tree_node(NULL, value));
-    if (value < (*tree)->n)
-    {
-        if ((*tree)->left == NULL)
-            return ((*tree)->left = binary_tree_node(*tree, value));
-        else
-            return (bst_insert(&((*tree)->left), value));
-    }
-    else if (value > (*tree)->n)
-    {
-        if ((*tree)->right == NULL)
-            return ((*tree)->right = binary_tree_node(*tree, value));
-        else
-            return (bst_insert(&((*tree)->right), value));
-    }
-    return (NULL);
+		return (*tree = binary_tree_node(NULL, value));
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+			return ((*tree)->left = binary_tree_node(*tree, value));
+		else
+			return (bst_insert(&((*tree)->left), value));
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+			return ((*tree)->right = binary_tree_node(*tree, value));
+		else
+			return (bst_insert(&((*tree)->right), value));
+	}
+	return (NULL);
 }
