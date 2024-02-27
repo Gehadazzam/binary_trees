0f33ae1c2c43d475cbff506c7b4a6e75b5d7d93a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-27 07:36:10 +0200

    binary

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
+
 /**
-*binary_tree_sibling - find the sibiling of node
-*
-*@node:pointer to the node we will check
-*
-*Return: pointer to the node_sibling or null 
-*/
+ * binary_tree_sibling - find the sibiling of node
+ * @node:pointer to the node we will check
+ * Return: pointer to the node_sibling or null 
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else if (node == node->parent->right)
+		return (node->parent->left);
+	else
+		return (NULL);
 }
