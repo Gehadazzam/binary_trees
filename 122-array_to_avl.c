1eef0049e917efab4ff77491218fb707bf71b641
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-28 15:35:49 +0200

    binary

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
 *array_to_avl - from an array build an avl tree
 *@array: array to convert
@@ -7,5 +8,10 @@
 */
 avl_t *array_to_avl(int *array, size_t size)
 {
+	bst_t *root = NULL;
+	size_t i;
 
+	for (i = 0; i < size; i++)
+		avl_insert(&root, array[i]);
+	return (root);
 }
