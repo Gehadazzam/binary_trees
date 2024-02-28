af27c0de1d504fbd5d31e1b936fb2d3cb3fa1597
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-29 01:29:00 +0200

    binary

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,11 +1,67 @@
 #include "binary_trees.h"
+
 /**
-*heap_insert - insert a value to heap tree
-*@root: pointer to the root
-*@value: value to be added
-*Return: pointer to the root
-*/
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left, height_right;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (height_left < height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
+}
+
+/**
+ * heap_insert - Inserts a value in a Max Binary Heap
+ * @root: Double pointer to the root node of the Heap
+ * @value: Value to store in the node to be inserted
+ * Return: Pointer to the created node, or NULL on failure
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
+	heap_t *new_node, *parent;
 
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
+
+	parent = *root;
+	while (parent->left != NULL && parent->right != NULL)
+	{
+		if (binary_tree_height(parent->left) <= binary_tree_height(parent->right))
+			parent = parent->left;
+		else
+			parent = parent->right;
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+
+	while (new_node->parent != NULL && new_node->n > new_node->parent->n)
+	{
+		new_node->n = new_node->parent->n;
+		new_node->parent->n = value;
+		new_node = new_node->parent;
+	}
+
+	return (new_node);
 }
+
