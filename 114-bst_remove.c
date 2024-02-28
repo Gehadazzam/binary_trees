16f04b0b23aff5410c146e7314c5d21324ec6a8f
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-28 02:57:05 +0200

    binary

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
 *bst_remove - remove a node from a bst
 *@root: pointer to the root
@@ -7,5 +8,36 @@
 */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    
+	bst_t *child;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL)
+		{
+			child = root->right;
+			free(root);
+			return (child);
+		}
+		else if (root->right == NULL)
+		{
+			child = root->left;
+			free(root);
+			return (child);
+		}
+		else
+		{
+			child = root->right;
+			while (child->left  != NULL)
+				child = child->left;
+			root->n = child->n;
+			root->right = bst_remove(root->right, child->n);
+		}
+	}
+	return (root);
 }
