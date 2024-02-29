d4d27fb5cca9b84f1b3243e9de89c65f96c8add4
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-29 04:36:44 +0200

    binary

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -7,15 +7,15 @@
  */
 size_t heap_tree_size(const binary_tree_t *tree)
 {
-    size_t size_left = 0, size_right = 0;
+	size_t size_left = 0, size_right = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left)
-        size_left = 1 + heap_tree_size(tree->left);
-    if (tree->right)
-        size_right = 1 + heap_tree_size(tree->right);
-    return (size_left + size_right);
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		size_left = 1 + heap_tree_size(tree->left);
+	if (tree->right)
+		size_right = 1 + heap_tree_size(tree->right);
+	return (size_left + size_right);
 }
 
 /**
@@ -25,18 +25,18 @@ size_t heap_tree_size(const binary_tree_t *tree)
  */
 size_t heap_tree_height(const heap_t *tree)
 {
-    size_t height_left = 0, height_right = 0;
+	size_t height_left = 0, height_right = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left)
-        height_left = heap_tree_height(tree->left);
-    if (tree->right)
-        height_right = heap_tree_height(tree->right);
-    if (height_left > height_right)
-        return (height_left + 1);
-    else
-        return (height_right + 1);
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		height_left = heap_tree_height(tree->left);
+	if (tree->right)
+		height_right = heap_tree_height(tree->right);
+	if (height_left > height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
 }
 
 /**
@@ -47,13 +47,13 @@ size_t heap_tree_height(const heap_t *tree)
  */
 void heap_tree_preorder(heap_t *root, heap_t **tree, size_t h)
 {
-    if (root == NULL)
-        return;
-    if (h == 1)
-        *tree = root;
-    h--;
-    heap_tree_preorder(root->left, tree, h);
-    heap_tree_preorder(root->right, tree, h);
+	if (root == NULL)
+		return;
+	if (h == 1)
+		*tree = root;
+	h--;
+	heap_tree_preorder(root->left, tree, h);
+	heap_tree_preorder(root->right, tree, h);
 }
 
 /**
@@ -62,32 +62,32 @@ void heap_tree_preorder(heap_t *root, heap_t **tree, size_t h)
  */
 void heap_helper(heap_t *tree)
 {
-    heap_t *new, *old;
-    int value;
+	heap_t *new, *old;
+	int value;
 
-    if (tree == NULL)
-        return;
-    new = tree;
-    while (1)
-    {
-        if (new->left == NULL)
-            break;
-        if (new->right == NULL)
-            old = new->left;
-        else
-        {
-            if (new->left->n > new->right->n)
-                old = new->left;
-            else
-                old = new->right;
-        }
-        if (new->n > old->n)
-            break;
-        value = new->n;
-        new->n = old->n;
-        old->n = value;
-        new = old;
-    }
+	if (tree == NULL)
+		return;
+	new = tree;
+	while (1)
+	{
+		if (new->left == NULL)
+			break;
+		if (new->right == NULL)
+			old = new->left;
+		else
+		{
+			if (new->left->n > new->right->n)
+				old = new->left;
+			else
+				old = new->right;
+		}
+		if (new->n > old->n)
+			break;
+		value = new->n;
+		new->n = old->n;
+		old->n = value;
+		new = old;
+	}
 }
 
 /**
@@ -97,26 +97,25 @@ void heap_helper(heap_t *tree)
  */
 int heap_extract(heap_t **root)
 {
-    heap_t *tree;
-    int value;
+	heap_t *tree;
+	int value;
 
-    if (root == NULL || *root == NULL)
-        return (0);
-    value = (*root)->n;
-    if ((*root)->left == NULL && (*root)->right == NULL)
-    {
-        free(*root);
-        *root = NULL;
-        return (value);
-    }
-    heap_tree_preorder(*root, &tree, heap_tree_height(*root));
-    (*root)->n = tree->n;
-    if (tree->parent->left == tree)
-        tree->parent->left = NULL;
-    else
-        tree->parent->right = NULL;
-    free(tree);
-    heap_helper(*root);
-    return (value);
+	if (root == NULL || *root == NULL)
+		return (0);
+	value = (*root)->n;
+	if ((*root)->left == NULL && (*root)->right == NULL)
+	{
+		free(*root);
+		*root = NULL;
+		return (value);
+	}
+	heap_tree_preorder(*root, &tree, heap_tree_height(*root));
+	(*root)->n = tree->n;
+	if (tree->parent->left == tree)
+		tree->parent->left = NULL;
+	else
+		tree->parent->right = NULL;
+	free(tree);
+	heap_helper(*root);
+	return (value);
 }
-
